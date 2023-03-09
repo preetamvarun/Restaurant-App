8e543062a571b1ea12ea70339087d1b8255be02b
Preetam Varun <preetamarun99@gmail.com>
2023-03-09 21:54:22 +0000

    put navigator component in body component

--- src/Components/BodyComponent/Body.js
+++ src/Components/BodyComponent/Body.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Navigator from './NavigatorComponent/Navigator';
 import Searchbar from './SearchBarComponent/Searchbar';
 
 const Body = () => {
   return (
     <React.Fragment>
+      <Navigator />
       <Searchbar />
     </React.Fragment>
   );
--- src/Components/BodyComponent/SearchBarComponent/Searchbar.js
+++ src/Components/BodyComponent/SearchBarComponent/Searchbar.js
@@ -1,7 +1,29 @@
+import { useState } from 'react';
+import './Searchbar.css';
+
 const Searchbar = () => {
+  const [searchFood, setSearchFood] = useState('');
+
+  const handleChange = (evt) => {
+    setSearchFood(evt.target.value);
+  };
+
   return (
-    <div>
-      <p>This is a search bar</p>
+    <div className='searchbar'>
+      <div className='CravingsDiv'>
+        <p>
+          Search For Your Cravings
+          <span style={{ fontSize: '2rem' }}>🍜🍕</span>
+        </p>
+        <div className='inputDiv'>
+          <input
+            type={'text'}
+            placeholder={'search food...'}
+            value={searchFood}
+            onChange={handleChange}
+          />
+        </div>
+      </div>
     </div>
   );
 };
