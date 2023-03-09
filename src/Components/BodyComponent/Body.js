af536e3aad32f824540ef60a8cf2cd2d6c608806
Preetam Varun <preetamarun99@gmail.com>
2023-03-09 14:33:14 +0000

    created search bar component

--- src/Components/BodyComponent/Body.js
+++ src/Components/BodyComponent/Body.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Searchbar from './SearchBarComponent/Searchbar';
 
 const Body = () => {
-  return <React.Fragment></React.Fragment>;
+  return (
+    <React.Fragment>
+      <Searchbar />
+    </React.Fragment>
+  );
 };
 
 export default Body;
---
+++ src/Components/BodyComponent/SearchBarComponent/Searchbar.js
@@ -0,0 +1,9 @@
+const Searchbar = () => {
+  return (
+    <div>
+      <p>This is a search bar</p>
+    </div>
+  );
+};
+
+export default Searchbar;
