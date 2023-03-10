dd7d0d9e332d3c6ea04e94a510e7e187cdb3e83e
Preetam Varun <preetamarun99@gmail.com>
2023-03-10 19:00:50 +0000

    "Got restaurants data"

--- src/Components/BodyComponent/ShowRestaurants.js
+++ src/Components/BodyComponent/ShowRestaurants.js
@@ -1,11 +1,26 @@
-import GetAllRestaurants from '../../utils/GetAllRestaurants';
 import { useEffect, useState } from 'react';
+import useGetAllRestaurants from '../../utils/useGetAllRestaurants';
+import './ShowRestaurant.css';
 
 const ShowRestaurants = () => {
-  const [allRestaurants, setAllRestaurants] = useState();
+  const allRestaurants = useGetAllRestaurants();
+
+  const [filteredRestaurants, setfilteredRestaurants] = useState();
+
+  useEffect(() => {
+    setfilteredRestaurants(allRestaurants);
+  }, [allRestaurants]);
+
+  const arr = allRestaurants?.data?.cards?.filter(
+    (eachCard) => eachCard?.cardType !== 'carousel'
+  )[0];
+
+  const oneRestaurant = arr?.data?.data?.cards[0];
+
+  console.log(oneRestaurant?.data);
 
   return (
-    <div>
+    <div className='allRestaurants'>
       <h1>About to show some Restaurants</h1>
     </div>
   );
--- src/utils/useGetAllRestaurants.js
+++ src/utils/useGetAllRestaurants.js
@@ -2,19 +2,19 @@ import { useEffect, useState } from 'react';
 import { API_URL } from './Config';
 
 const GetAllRestaurants = () => {
+  const [allRestaurants, setAllRestaurants] = useState('');
+
   async function getAllRestaurants() {
     const res = await fetch(API_URL);
     const resData = await res.json();
-    SetAllRestaurants(resData);
+    setAllRestaurants(resData);
   }
-
-  const [AllRestaurants, SetAllRestaurants] = useState();
 
   useEffect(() => {
     getAllRestaurants();
   }, []);
 
-  return AllRestaurants;
+  return allRestaurants;
 };
 
 export default GetAllRestaurants;
