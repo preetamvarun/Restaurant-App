b4ea937cf7b054478c286f6823d89f2b5e4a2ad8
Preetam Varun <preetamarun99@gmail.com>
2023-03-11 17:31:50 +0000

    displayed each restaurant data. Time to style

--- src/Components/BodyComponent/Restaurant.js
+++ src/Components/BodyComponent/Restaurant.js
@@ -1,7 +1,42 @@
-const Restaurant = ({ name }) => {
+import { IMG_URL } from '../../utils/Config';
+
+const Restaurant = ({
+  name,
+  cuisines,
+  avgRating,
+  slaString,
+  costForTwoString,
+  meta,
+  cloudinaryImageId,
+}) => {
   return (
-    <div>
-      <p>{name}</p>
+    <div style={{ backgroundColor: 'green', margin: '1rem' }}>
+      <div>
+        <img src={IMG_URL + cloudinaryImageId} alt={name} />
+      </div>
+      <div>
+        <p>{name}</p>
+        <div>
+          {cuisines?.map((cuisine) => (
+            <p style={{ display: 'inline-block' }} key={cuisine}>
+              {cuisine}
+            </p>
+          ))}
+        </div>
+      </div>
+      <div>
+        <span>{avgRating}</span>
+        <span>.</span>
+        <span>{slaString}</span>
+        <span>.</span>
+        <span>{costForTwoString}</span>
+      </div>
+      {/* Here You have to draw a horizontal line */}
+      <div>
+        <span>{meta}</span>
+      </div>
+      {/* Here You have to draw a horizontal line */}
+      <p>Quick View</p>
     </div>
   );
 };
--- src/Components/BodyComponent/ShowRestaurants.js
+++ src/Components/BodyComponent/ShowRestaurants.js
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import useGetAllRestaurants from '../../utils/useGetAllRestaurants';
 import Restaurant from './Restaurant';
 import './ShowRestaurant.css';
@@ -6,19 +5,17 @@ import './ShowRestaurant.css';
 const ShowRestaurants = () => {
   const allRestaurants = useGetAllRestaurants();
 
-  const [filteredRestaurants, setfilteredRestaurants] = useState();
-
-  useEffect(() => {
-    setfilteredRestaurants(allRestaurants);
-  }, [allRestaurants]);
-
   const arrOfRestaurants = allRestaurants?.data?.cards?.filter(
     (eachCard) => eachCard?.cardType !== 'carousel'
   )[0]?.data?.data?.cards;
 
-  console.log(arrOfRestaurants);
-
-  return <div className='allRestaurants'></div>;
+  return (
+    <div className='allRestaurants'>
+      {arrOfRestaurants?.map((restaurant) => (
+        <Restaurant {...restaurant?.data} key={restaurant?.data?.id} />
+      ))}
+    </div>
+  );
 };
 
 export default ShowRestaurants;
