28782006a536e377c8c9c6cab627eb095dd82fd4
Preetam Varun <preetamarun99@gmail.com>
2023-03-16 18:27:46 +0000

    Swiggy's public api is having a glitch

--- src/Components/RestaurantMenu/RestaurantMenu.js
+++ src/Components/RestaurantMenu/RestaurantMenu.js
@@ -8,34 +8,43 @@ const RestaurantMenu = () => {
   const { id } = useParams();
   const [restaurantMenu, setRestaurantMenu] = useState('');
 
-  const menuData = useRestaurantMenu(id);
-
-  const menuObj = restaurantMenu?.data?.menu?.items;
+  const menuData = useRestaurantMenu(id)?.data?.cards;
 
   useEffect(() => {
     setRestaurantMenu(menuData);
   }, [menuData]);
 
-  // restaurantMenu?.data?.menu?.items
-  // Object.entries(restaurantMenu?.data?.menu?.items)?.map(([key, value]) =>
-  //   console.log(key[value])
-  // );
+  const restaurantMenus =
+    restaurantMenu &&
+    restaurantMenu[
+      restaurantMenu.length - 1
+    ].groupedCard?.cardGroupMap?.REGULAR?.cards
+      ?.filter((eachCard) => eachCard?.card?.card?.itemCards)
+      .map((a) => a?.card?.card?.itemCards)
+      .flat(1)
+      .map((a) => a?.card?.info);
 
   return (
     <div className='Main-Menu'>
       <div className='Intro-Div'>
         <div className='First-Intro'>
-          <p>{restaurantMenu?.data?.name}</p>
-          <p>{restaurantMenu?.data?.cuisines.join(' , ')}</p>
+          <p>{restaurantMenu && restaurantMenu[0]?.card?.card?.info?.name}</p>
           <p>
-            {restaurantMenu?.data?.locality} ,{' '}
-            {restaurantMenu?.data?.sla?.lastMileDistanceString}
+            {restaurantMenu &&
+              restaurantMenu[0]?.card?.card?.info?.cuisines.join(' , ')}
+          </p>
+          <p>
+            {restaurantMenu && restaurantMenu[0]?.card?.card?.info?.locality} ,{' '}
+            {restaurantMenu &&
+              restaurantMenu[0]?.card?.card?.info?.sla?.lastMileTravelString}
           </p>
         </div>
         <div className='Second-Intro' style={{ fontSize: '.85rem' }}>
           <div style={{ color: '#3D9B6D', fontSize: '1rem' }}>
             <i className='fa-solid fa-star'></i>
-            <p>{restaurantMenu?.data?.avgRating}</p>
+            <p>
+              {restaurantMenu && restaurantMenu[0]?.card?.card?.info?.avgRating}
+            </p>
           </div>
           <hr
             style={{
@@ -46,17 +55,21 @@ const RestaurantMenu = () => {
             }}
           />
           <p style={{ color: '#848591' }}>
-            {restaurantMenu?.data?.totalRatingsString}
+            {restaurantMenu &&
+              restaurantMenu[0]?.card?.card?.info?.totalRatingsString}
           </p>
         </div>
       </div>
+
       <hr style={{ marginTop: '1rem' }} />
-      <div className='Recommended-Restaurants'>
-        {menuObj &&
-          Object.entries(menuObj).map(([key, value]) => (
-            <RecommedRestaurant key={key} {...value} />
-          ))}
-      </div>
+      {
+        <div className='Recommended-Restaurants'>
+          {restaurantMenus &&
+            restaurantMenus.map((restaurantMenu) => (
+              <RecommedRestaurant {...restaurantMenu} key={restaurantMenu.id} />
+            ))}
+        </div>
+      }
     </div>
   );
 };
