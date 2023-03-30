2402969ce2e9509e03981f8f2610bcae96bfdb6d
Preetam Varun <preetamarun99@gmail.com>
2023-03-30 19:14:44 +0100

    basic style on not found .js

--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -3,7 +3,7 @@ import React from 'react';
 const NotFound = () => {
   return (
     <div>
-      <h1>404 - Page Not Found</h1>
+      <h1 style={{ margin: '1rem' }}>404 - Page Not Found</h1>
       <p>The requested page could not be found.</p>
     </div>
   );
