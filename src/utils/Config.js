73cb942a9d2ebe47eed7f8e78416e42f36e9ff9a
Preetam Varun <preetamarun99@gmail.com>
2023-03-30 19:01:12 +0100

    remove cors anywhere for image upload

--- src/utils/Config.js
+++ src/utils/Config.js
@@ -1,7 +1,7 @@
 export const API_URL =
   'https://corsanywhere.herokuapp.com/https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&page_type=DESKTOP_WEB_LISTING';
 export const IMG_URL =
-  'https://corsanywhere.herokuapp.com/https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/';
+  'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/';
 
 export const FETCH_MENU_URL =
   'https://corsanywhere.herokuapp.com/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=11.1271225&lng=78.6568942&restaurantId=';
